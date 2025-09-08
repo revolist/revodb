9e8e00c879b1f77ca233610337d6f1777d1fae75
Pranav C <pranavxc@gmail.com>
2025-09-08 08:06:57 +0000

    feat: handle filter, sort and group by

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -6092,9 +6092,9 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
         c.uidt === UITypes.LastModifiedTime &&
         (c.system ||
           (c.column_name &&
-            (
-              c.colOptions as LastModColumnOptions
-            )?.triggerColumnIds?.some((id) => updatedColIds.includes(id)))),
+            (c.colOptions as LastModColumnOptions)?.triggerColumnIds?.some(
+              (id) => updatedColIds.includes(id),
+            ))),
     );
 
     const lastModifiedByColumns = columns.filter(
@@ -6102,9 +6102,9 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
         c.uidt === UITypes.LastModifiedBy &&
         (c.system ||
           (c.column_name &&
-            (
-              c.colOptions as LastModColumnOptions
-            )?.triggerColumnIds?.some((id) => updatedColIds.includes(id)))),
+            (c.colOptions as LastModColumnOptions)?.triggerColumnIds?.some(
+              (id) => updatedColIds.includes(id),
+            ))),
     );
 
     for (const lastModifiedTimeColumn of lastModifiedTimeColumns) {
@@ -6375,14 +6375,12 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
           column.uidt,
         ) &&
         column.column_name &&
-        (column.colOptions as LastModColumnOptions)?.triggerColumnIds
-          ?.length &&
+        (column.colOptions as LastModColumnOptions)?.triggerColumnIds?.length &&
         !isInsertData
       ) {
         // check if any of the tracked columns are updating
-        const trackColumnIds = (
-          column.colOptions as LastModColumnOptions
-        )?.triggerColumnIds;
+        const trackColumnIds = (column.colOptions as LastModColumnOptions)
+          ?.triggerColumnIds;
 
         if (
           trackColumnIds.every((id) =>
