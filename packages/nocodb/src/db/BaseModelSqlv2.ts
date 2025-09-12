49066081473d936e7bca65b3e76fce36ee76e57a
Pranav C <pranavxc@gmail.com>
2025-09-12 08:59:19 +0000

    fix: handle empty string value

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -26,6 +26,7 @@ import {
   NcApiVersion,
   NcErrorType,
   ncIsNull,
+  ncIsNullOrUndefined,
   ncIsObject,
   ncIsUndefined,
   PermissionEntity,
@@ -6572,7 +6573,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
           column.uidt,
         )
       ) {
-        if (data[column.column_name]) {
+        if (!ncIsNullOrUndefined(data[column.column_name])) {
           const userIds = [];
 
           if (
